6e14a327dceb67a673de27054b7c8a39981345f2
sola <aakindelen132@gmail.com>
2025-12-15 16:07:34 -0600

    added data and first command

--- Formula1.py
+++ Formula1.py
@@ -1,5 +1,36 @@
 import sqlite3
+import pandas as pd
 import requests 
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect('f1_data.db')
+
+def import_csv():
+    csv_files = [
+        'circuits',
+        'constructor_results',
+        'constructor_standings',
+        'constructors',
+        'driver_standings',
+        'drivers',
+        'lap_times',
+        'pit_stops',
+        'races',
+        'results',
+        'seasons',
+        'sprint_results',
+        'status'
+    ]
+
+    for file in csv_files:
+        try:
+            df = pd.read_csv(f'data/{file}.csv')
+            df.to_sql(file, conn, if_exists='replace', index=False)
+        except Exception as e:
+            print(f"Error importing {file}.csv: {e}")
+
+    conn.commit()
+    print("All CSVs imported successfully!")
 
 # fetch year for given data set 
 # returns a dictionary rather than the given json format
@@ -30,6 +61,41 @@ def fetch_pitsops(year, round_num):
     data = response.json()
     return data
 
+
+def search_driver_team(conn):
+    choice = input("Selct 'd' for diver and 't' for team ")
+    print("")
+    cursor = conn.cursor()
+    
+    if choice == 'd':
+        driver_name = input("Enter driver surname (use % for wildcard): ")
+        cursor.execute("""
+            SELECT driverID, driverRef, forename, surname, nationality, dob
+            FROM drivers
+            WHERE surname LIKE ?
+            ORDER BY surname ASC
+            """, [driver_name])
+        
+        rows = cursor.fetchall()
+        if len(rows) == 0:
+            print("No Drivers Found!")
+        else:
+            for d in rows:
+                print(f" {d[0]}: {d[2]} {d[3]} ({d[4]})")
+        
+    elif choice == 't':
+        pass
+    else:
+        print("Invalid choice")
+
+
+
+
 if __name__ == "__main__":
-    data = fetch_race_results(2024)
-    print(data)
+   import_csv()
+   #data = fetch_race_results(2024)
+   #print(data)
+
+   search_driver_team(conn)
+
+   conn.close()
