a1f761430b705f6f2cf8183c684707a2e8dc6f0d
sola <aakindelen132@gmail.com>
2025-12-20 15:08:09 -0600

    comparisons

--- Formula1.py
+++ Formula1.py
@@ -106,7 +106,8 @@ def top_ten_constructors(conn):
     cursor = conn.cursor()
 
     cursor.execute(""" 
-                SELECT name, SUM(wins) as total_wins, sum(points) as total_points               FROM constructors
+                SELECT name, SUM(wins) as total_wins, sum(points) as total_points               
+                FROM constructors
                 JOIN constructor_standings 
                 ON constructors.constructorId = constructor_standings.constructorId
                 GROUP BY name
@@ -192,6 +193,131 @@ def most_constructor_championships(conn):
     
     print("")
     
+def season_points(conn):
+    driver_name = input("Enter driver surname: ")
+    year = input("Enter the season(year): ")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT drivers.forename, drivers.surname, races.round, races.name, driver_standings.points
+    FROM driver_standings
+    JOIN drivers ON driver_standings.driverId = drivers.driverId
+    JOIN races ON driver_standings.raceId = races.raceId
+    WHERE drivers.surname LIKE ? AND races.year LIKE ?
+    ORDER BY races.date ASC
+    """, [driver_name, year])
+
+    rows = cursor.fetchall()
+
+    if len(rows) == 0:
+        print("No races found!")
+    else:
+        print(f"{rows[0][0]} {rows[0][1]} - {year} Season Points Progression:\n")
+        
+        for r in rows:
+            print(f"Round {r[2]}: {r[3]:<30} - {r[4]} points")
+
+    plot = input("\nPlot? (y/n)")
+
+    if plot == 'y':
+        round = [r[2] for r in rows]
+        points = [r[4] for r in rows]
+
+    
+        plt.xlabel("Race Round")
+        plt.ylabel("Total Points")
+
+        plt.title(f"{rows[0][0]} {rows[0][1]} - {year} Season")
+        plt.plot(round, points, marker='o')
+
+        plt.grid(True)
+        plt.show()
+    else:
+        pass
+
+def two_drivers_comparison(conn):
+    driver_1 = input("Enter driver 1 surname: ")
+    driver_2 = input("Enter driver 2 surname: ")
+    year = input("Enter the season(year): ")
+
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT drivers.forename, drivers.surname, races.round, races.name, driver_standings.points
+        FROM driver_standings
+        JOIN drivers ON driver_standings.driverId = drivers.driverId
+        JOIN races ON driver_standings.raceId = races.raceId
+        WHERE drivers.surname LIKE ? AND races.year LIKE ?
+        ORDER BY races.date ASC
+    """, [driver_1, year])
+
+    driver1_rows = cursor.fetchall()
+
+    cursor.execute("""
+        SELECT drivers.forename, drivers.surname, races.round, races.name, driver_standings.points
+        FROM driver_standings
+        JOIN drivers ON driver_standings.driverId = drivers.driverId
+        JOIN races ON driver_standings.raceId = races.raceId
+        WHERE drivers.surname LIKE ? AND races.year LIKE ?
+        ORDER BY races.date ASC
+    """, [driver_2, year])
+
+    driver2_rows = cursor.fetchall()
+
+    if len(driver1_rows) == 0 or len(driver2_rows) == 0:
+        print("One or both drivers not found")
+        return
+    else: 
+        print(f"\n{driver1_rows[0][0]} {driver1_rows[0][1]} vs {driver2_rows[0][0]} {driver2_rows[0][1]} - {year}\n")
+
+        for d1, d2 in zip(driver1_rows, driver2_rows):
+            print(f"Round {d1[2]}: {d1[3]:<30} | {d1[0]} {d1[1]}: {d1[4]} pts | {d2[0]} {d2[1]}: {d2[4]} pts")
+
+    plot = input("\nPlot? (y/n) ")
+
+    if plot == 'y':
+        d1_round = [d[2] for d in driver1_rows]
+        d1_points = [d[4] for d in driver1_rows]
+        d2_round = [d[2] for d in driver2_rows]
+        d2_points = [d[4] for d in driver2_rows]
+    else:
+        pass
+
+    plt.plot(d1_round, d1_points, marker='o', label=f"{driver1_rows[0][0]} {driver1_rows[0][1]}")
+    plt.plot(d2_round, d2_points, marker='s', label=f"{driver2_rows[0][0]} {driver2_rows[0][1]}")
+    plt.xlabel("Race Round")
+    plt.ylabel("Total Points")
+    plt.title(f"{year} - {driver_1} vs. {driver_2}")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
+def fast_pit_stop(conn):
+    cursor = conn.cursor()
+    driver = input("Enter driver surname: ")
+    year = input("Enter the season(year): ")
+
+    cursor.execute("""
+    SELECT drivers.forename, drivers.surname, 
+            AVG(pit_stops.milliseconds) as avg_duration_ms, COUNT(*) as num_stops
+    FROM pit_stops
+    JOIN drivers ON pit_stops.driverId = drivers.driverId
+    JOIN races ON  pit_stops.raceId =  races.raceId
+    WHERE races.year LIKE ? AND drivers.surname LIKE  ?
+    GROUP BY pit_stops.driverId
+    ORDER BY avg_duration_ms ASC
+    """, [year, driver])
+
+    rows = cursor.fetchall()
+
+    if len(rows) == 0:
+        print("Invalid Year")
+    else:
+        print(f"\n{year} Average Pit Stop Duration:\n")
+        for r in rows:
+            avg_seconds = r[2] / 1000
+            print(f"{r[0]} {r[1]} : {avg_seconds:.3f} seconds ({r[3]} stops)")
+
 if __name__ == "__main__":
    import_csv() 
    #data = fetch_race_results(2024)
@@ -199,7 +325,9 @@ if __name__ == "__main__":
    #top_ten_constructors(conn)
    #top_20_gp(conn)
    #search_driver_team(conn)
-   most_driver_championships(conn)
-   most_constructor_championships(conn)
-   conn.close()
+   #most_driver_championships(conn)
+   #most_constructor_championships(conn)
+   fast_pit_stop(conn)
+   #two_drivers_comparison(conn)
 
+   #season_points(conn)   
